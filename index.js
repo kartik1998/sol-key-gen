d4ab2e0669114c888dbee10ca14a51b9cd7c5a01
kartik1998 <kartikrawat61@gmail.com>
2021-10-09 14:09:54 +0530

    Update: create file paths for sol keys

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 const express = require("express");
 const app = express();
+const path = require("path");
+const utils = require("./lib/utils");
+
 const PORT = process.env.PORT || 3000;
 
 app.get("/ping", (_, res) => res.send("ping"));
+
+app.get("/generatePair", (_, res) => {
+  const filePath = path.resolve("./creds") + utils.computeRandomString(40);
+  res.json({ filePath: filePath });
+});
 
 app.listen(PORT, () => {
   console.log(`connection established on port ${PORT}`);
